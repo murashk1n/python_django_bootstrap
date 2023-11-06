b46a49e71a262d34ea23493eda731a0b6fa39375
Andrei Murashkin <amuraskin323@gmail.com>
2023-11-06 23:01:20 +0200

    add form, detail page

--- travel/urls.py
+++ travel/urls.py
@@ -22,6 +22,6 @@ from travel.views import home, about
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cities/', include(('cities.urls', 'cities'))),
-    path('home/', home),
+    path('', home, name='home'),
     path('about/', about)
 ]
