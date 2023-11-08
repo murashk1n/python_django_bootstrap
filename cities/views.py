aab23bf60479d6e1226e8263de499d9d43fa1cd5
Andrei Murashkin <amuraskin323@gmail.com>
2023-11-08 21:18:03 +0200

    add pagination

--- cities/views.py
+++ cities/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, ListView
 
-from cities.forms import HtmlForm, CityForm
+from cities.forms import CityForm
 from cities.models import City
 
 __all__ = (
-    'home', 'CityDetailView', 'CityCreateView', 'CityUpdateView', 'CityDeleteView'
+    'home', 'CityDetailView', 'CityCreateView',
+    'CityUpdateView', 'CityDeleteView', 'CityListView'
 )
 
 
@@ -25,7 +27,10 @@ def home(request, pk=None):
     # return render(request, 'cities/detail.html', context)
     form = CityForm()
     qs = City.objects.all()
-    context = {'objects_list': qs, 'form': form}
+    lst = Paginator(qs, 2)
+    page_number = request.GET.get('page')
+    page_obj = lst.get_page(page_number)
+    context = {'page_obj': page_obj, 'form': form}
     return render(request, 'cities/home.html', context)
 
 
@@ -50,8 +55,14 @@ class CityUpdateView(UpdateView):
 
 class CityDeleteView(DeleteView):
     model = City
-    # template_name = 'cities/delete.html'
+    template_name = 'cities/delete.html'
     success_url = reverse_lazy('cities:home')
 
     def get(self, request, *args, **kwargs):
         return self.post(request, *args, **kwargs)
+
+
+class CityListView(ListView):
+    paginate_by = 2
+    model = City
+    template_name = 'cities/home.html'
