89df886c1bfb3f001fb508a00a6e7f7c67bf23c4
Andrei Murashkin <amuraskin323@gmail.com>
2023-11-08 09:21:06 +0200

    add update view

--- cities/urls.py
+++ cities/urls.py
@@ -5,5 +5,6 @@ from cities.views import *
 urlpatterns = [
     path('', home, name='home'),
     path('detail/<int:pk>/', CityDetailView.as_view(), name='detail'),
+    path('update/<int:pk>/', CityUpdateView.as_view(), name='update'),
     path('add/', CityCreateView.as_view(), name='create'),
 ]
