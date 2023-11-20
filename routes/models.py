bc95b1b2636e71275cd95fb3e0fb3c5ebcc87163
Andrei Murashkin <amuraskin323@gmail.com>
2023-11-20 13:28:32 +0200

    add routes page

--- routes/models.py
+++ routes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from cities.models import City
+
+
+class Route(models.Model):
+    name = models.CharField(max_length=50, unique=True,
+                            verbose_name='Route')
+    travel_times = models.PositiveSmallIntegerField(verbose_name='Total duration of the trip')
+    from_city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='route_from_city_set',
+                                  verbose_name='From city')
+    to_city = models.ForeignKey('cities.City', on_delete=models.CASCADE, related_name='route_to_city_set',
+                                verbose_name='To city')
+    trains = models.ManyToManyField('trains.Train', verbose_name='List of trains')
+
+    def __str__(self):
+        return f'Route {self.name} from {self.from_city}'
+
+    class Meta:
+        verbose_name = 'Route'
+        verbose_name_plural = 'Routes'
+        ordering = ['travel_times']
--- travel/views.py
+++ travel/views.py
@@ -6,6 +6,7 @@ def home(request):
     name = 'Andrei'
     return render(request, "home.html", {'name': name})
 
+
 def about(request):
     name = 'About'
     return render(request, "about.html", {'name': name})
